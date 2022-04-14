5c3eed8318e37751801f9b6dbddef85e0073d126
Vyacheslav Shirnin <themindsaund@gmail.com>
2022-04-15 01:13:42 +0700

    установка Laravel + шаблон AdminLTE

---
+++ webpack.mix.js
@@ -0,0 +1,38 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles([
+    <!-- Font Awesome -->
+    'resources/assets/admin/plugins/fontawesome-free/css/all.min.css',
+    <!-- Theme style -->
+    'resources/assets/admin/css/adminlte.min.css'
+], 'public/assets/admin/css/admin.css');
+
+mix.scripts([
+    <!-- jQuery -->
+    'resources/assets/admin/plugins/jquery/jquery.min.js',
+    <!-- Bootstrap 4 -->
+    'resources/assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    <!-- AdminLTE App -->
+    'resources/assets/admin/js/adminlte.min.js',
+    <!-- AdminLTE for demo purposes -->
+    'resources/assets/admin/js/demo.js'
+], 'public/assets/admin/js/admin.js');
+
+mix.copyDirectory('resources/assets/admin/img', 'public/assets/admin/img');
+mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'public/assets/admin/webfonts');
+
+mix.copy('resources/assets/admin/css/adminlte.min.css.map', 'public/assets/admin/css/adminlte.min.css.map');
