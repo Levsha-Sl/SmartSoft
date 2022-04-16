6d46b32cc1ae4311554b9c010fe7d7f6daf793f3
Vyacheslav Shirnin <themindsaund@gmail.com>
2022-04-17 00:35:32 +0700

    added library sluggable

--- app/Tag.php
+++ app/Tag.php
@@ -2,9 +2,29 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
